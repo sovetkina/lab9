7260d935951fc2f66b1cc58c44cc9b6062c3db8e
sovetkina <72756600+sovetkina@users.noreply.github.com>
2021-12-12 21:54:13 +0300

    Add files via upload

---
+++ lab9.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Лабораторная работа</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<form method="POST" action="">
+			<a>Введите число n</a><input type="text1" name="n"><br/>
+			<a>Введите число x</a><input type="text2" name="x"><br/>
+			<input type="submit" value="Отправить">
+		</form>
+		<?php
+			$n=$_POST['n'];
+			$x=$_POST['x'];
+			$sum=0;
+			for($i=0; $i<=$n; $i++){
+				$f=pow(-1, $n)*pow($x-1,$n+1)/($n+1);
+				$sum=$sum + $f;
+			}
+			echo $sum;
+		?>	
+	</body>
+</html>
